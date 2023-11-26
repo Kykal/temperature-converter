b10e71913205997048338983259c544fa347c40e
Kykal <benavides.alan@hotmail.com>
2023-11-26 15:14:49 -0600

    Update _KelvinOutput.tsx

--- app/components/_KelvinOutput.tsx
+++ app/components/_KelvinOutput.tsx
@@ -6,6 +6,10 @@
 import useTemperatureSearchParams from "../hooks/useTemperaturesParams";
 
 
+//Lib
+import fixNumber from "../lib/fixNumber";
+
+
 //Components
 import TemperatureOutput from "./TemperatureOutput";
 
@@ -15,12 +19,36 @@ const KelvinOutput = (): JSX.Element => {
 
 	const { temperature, sourceType } = useTemperatureSearchParams();
 	
+	let finalTemperature = 0;
+
+	switch (sourceType) {
+		case 'celsius': {
+			finalTemperature = fixNumber( temperature + 273.15 );
+			break;
+		};
+
+		case 'kelvin': {
+			finalTemperature = temperature;
+			break;
+		};
+
+		case 'fahrenheit': {
+			finalTemperature = fixNumber( (temperature - 32) * 5/9 + 273.15 );
+			break;
+		};
+
+		case 'rankine': {
+			finalTemperature = fixNumber( temperature * 5/9 );
+			break;
+		};
+	};
+
 
 	//Main component render
 	return (
 		<TemperatureOutput
 			id='kelvin'
-			temperature={temperature}
+			temperature={finalTemperature}
 			label='Kelvin'
 			acronym='K'
 		/>

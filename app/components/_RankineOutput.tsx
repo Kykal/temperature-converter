86c865aeacc1dfb7f3b5f105cd72e26bb6f38e40
Kykal <benavides.alan@hotmail.com>
2023-11-26 15:18:32 -0600

    Update _RankineOutput.tsx

--- app/components/_RankineOutput.tsx
+++ app/components/_RankineOutput.tsx
@@ -15,6 +15,30 @@ const RankineOutput = (): JSX.Element => {
 
 	const { temperature, sourceType } = useTemperatureSearchParams();
 
+	let finalTemperature = 0;
+
+	switch (sourceType) {
+		case 'celsius': {
+			finalTemperature = temperature + 273.15;
+			break;
+		};
+
+		case 'kelvin': {
+			finalTemperature = temperature;
+			break;
+		};
+
+		case 'fahrenheit': {
+			finalTemperature = (temperature - 32) * 5/9 + 273.15;
+			break;
+		};
+
+		case 'rankine': {
+			finalTemperature = temperature * 5/9;
+			break;
+		};
+	};
+
 
 	//Main component render
 	return (

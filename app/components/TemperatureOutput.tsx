d357a173fc8119482dda6af60add4b82fe0e1d85
Kykal <benavides.alan@hotmail.com>
2023-11-26 15:43:03 -0600

    Update TemperatureOutput.tsx

--- app/components/TemperatureOutput.tsx
+++ app/components/TemperatureOutput.tsx
@@ -1,6 +1,17 @@
+//NextJS
+'use client';
+
+
+//React
+import { useState } from "react";
+
+
 //MATERIAL DESIGN
 //Icons
-import { MdContentCopy } from "react-icons/md";
+import {
+	MdContentCopy,
+	MdCheck,
+} from "react-icons/md";
 
 
 //Typings
@@ -15,13 +26,19 @@ type TemperatureOutput = {
 //Main component content
 const TemperatureOutput = ({ id,temperature,label,acronym }: TemperatureOutput): JSX.Element => {
 
+	const [ clicked, setClicked ] = useState<boolean>(false);
+
+
 	const fixedTemperature = temperature.toFixed(3);
 
 	const output = `${fixedTemperature} ${acronym}`;
 
 	const onClickHandler = () => {
 		navigator.clipboard.writeText(fixedTemperature);
+		setClicked(true);
+		setTimeout( () => setClicked(false), 5_000 );
 	};
+
 
 
 	//Main component render
@@ -37,10 +54,17 @@ const TemperatureOutput = ({ id,temperature,label,acronym }: TemperatureOutput):
 			<output className='temperature-output' >
 				{output}
 			</output>
-			<MdContentCopy
-				className='temperature-copy-icon group-hover:scale-100'
-				aria-label='Copy temperature output'
-			/>
+			{clicked ? (
+				<MdCheck
+					className='temperature-copy-icon group-hover:scale-100 fill-green-500'
+					aria-label='Copied temperature output'
+				/>
+			) : (
+				<MdContentCopy
+					className='temperature-copy-icon group-hover:scale-100'
+					aria-label='Copy temperature output'
+				/>
+			)}
 		</div>
 	);
 };

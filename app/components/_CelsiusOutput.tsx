f037f74598c90f0d40550a44c08d5581ca94d698
Kykal <benavides.alan@hotmail.com>
2023-11-26 15:08:30 -0600

    Update _CelsiusOutput.tsx

--- app/components/_CelsiusOutput.tsx
+++ app/components/_CelsiusOutput.tsx
@@ -6,6 +6,10 @@
 import useTemperatureSearchParams from "../hooks/useTemperaturesParams";
 
 
+//Lib
+import fixNumber from "../lib/fixNumber";
+
+
 //Components
 import TemperatureOutput from "./TemperatureOutput";
 
@@ -15,11 +19,36 @@ const CelsiusOutput = (): JSX.Element => {
 
 	const { temperature, sourceType } = useTemperatureSearchParams();
 
+	let finalTemperature = 0;
+
+	switch (sourceType) {
+		case 'celsius': {
+			finalTemperature = temperature;
+			break;
+		};
+
+		case 'kelvin': {
+			finalTemperature = fixNumber(temperature - 273.15);
+			break;
+		};
+
+		case 'fahrenheit': {
+			finalTemperature = fixNumber((temperature - 32) * 5/9);
+			break;
+		};
+
+		case 'rankine': {
+			finalTemperature = fixNumber( (temperature - 491.67) * 5/9 );
+			break;
+		};
+	};
+
+
 	//Main component render
 	return (
 		<TemperatureOutput
 			id='celsius'
-			temperature={temperature}
+			temperature={finalTemperature}
 			label='Celsius'
 			acronym='ºC'
 		/>
